c4a73df74145fd0829468736ee047f5af53e56f0
sunht_孙恒涛 <sunht@fingard.com>
2022-04-21 14:47:45 +0800

    试验CompletableFuture delay效果，同mono delay

---
+++ src/test/kotlin/com/example/javaniodemo/niotest/FutureTest.java
@@ -0,0 +1,76 @@
+package com.example.javaniodemo.niotest;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.example.javaniodemo.demo.IoParallelUtil.countRequest;
+
+@Slf4j
+public class FutureTest {
+
+    @BeforeEach
+    public void setUp() {
+        // 限制ForkJoinPool的线程数，否则会创建和cpu核数相同的线程
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "2");
+    }
+
+    @SneakyThrows
+    @Test
+    public void testDelay() {
+        log.info("start");
+        final CompletableFuture<String> future = apiRequest()
+                .whenComplete((s, throwable) -> log.info(s));
+
+        future.get();
+    }
+
+    @Test
+    public void multiTest() throws Exception {
+        int parallelCount = 100;
+        int requestsPerParallel = 2;
+
+        AtomicInteger counter = new AtomicInteger(0);
+        long start = System.currentTimeMillis();
+        System.out.println("开始执行");
+
+        final CompletableFuture<Void> resultFuture = CompletableFuture.allOf(IntStream.rangeClosed(1, parallelCount)
+                        .boxed()
+                        .map(i -> {
+                            CompletableFuture<String> singleParallelFuture = CompletableFuture.completedFuture("");
+                            for (int j = 0; j < requestsPerParallel; j++) {
+                                singleParallelFuture = singleParallelFuture.thenCompose(s -> apiRequest()
+                                        .whenComplete((s1, throwable) -> {
+                                            Assertions.assertEquals("hello", s1);
+                                            countRequest(counter, i);
+                                        }));
+                            }
+                            return singleParallelFuture;
+                        })
+                        .collect(Collectors.toList())
+                        .toArray(new CompletableFuture[]{}))
+                .whenComplete((unused, throwable) -> {
+                    final long duration = (System.currentTimeMillis() - start) / 1000;
+                    System.out.println("请求成功：" + counter + "，耗时s：" + duration);
+                });
+
+        // 阻塞主线程到运行结束，实际服务端项目中不应该出现这个
+        resultFuture.get();
+    }
+
+    public CompletableFuture<String> apiRequest() {
+        return CompletableFuture.supplyAsync(() -> "hello",
+                CompletableFuture.delayedExecutor(5, TimeUnit.SECONDS));
+    }
+}
