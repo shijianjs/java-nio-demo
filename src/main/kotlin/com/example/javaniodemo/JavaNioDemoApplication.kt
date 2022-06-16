797d50dedf3053b9cd241978eeec28b19b64c5ef
sunht_孙恒涛 <sunht@fingard.com>
2022-06-16 09:22:49 +0800

    headerTest controller

--- src/main/kotlin/com/example/javaniodemo/JavaNioDemoApplication.kt
+++ src/main/kotlin/com/example/javaniodemo/JavaNioDemoApplication.kt
@@ -1,20 +1,25 @@
 package com.example.javaniodemo
 
 import kotlinx.coroutines.delay
+import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.WebRequest
 import kotlin.time.ExperimentalTime
 import kotlin.time.seconds
 
 @SpringBootApplication
 class JavaNioDemoApplication
 
+val log = KotlinLogging.logger { }
 fun main(args: Array<String>) {
     runApplication<JavaNioDemoApplication>(*args)
 }
@@ -34,7 +39,14 @@ class ServerController(){
         delay(5.seconds)
         return "hello"
     }
+
+    @RequestMapping("/headerTest")
+    suspend fun headerTest(req: ServerHttpRequest): Map<String, MutableList<String>> {
+        log.info { req.headers }
+        return req.headers.toMap()
+    }
 }
+
 
 @Configuration
 class Config {
